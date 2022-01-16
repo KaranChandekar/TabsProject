95953ce32049f989d351e4d5c75301a97f2d5bca
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-16 16:21:08 +0530

    remove active from other buttons

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const btns = document.querySelectorAll(".tab-btn");
 const about = document.querySelector(".about");
 const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove active from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+  }
+});
